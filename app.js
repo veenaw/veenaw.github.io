ed90dc2383f5a961a4d3e9b9d8a95a2715dc2a21
veenaw <vwagle2@gmail.com>
2020-04-11 21:54:36 +0530

    sq install changes

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // register service worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/veenaw.github.io/sw.js', { scope: '/veenaw.github.io/' });
-        
-        //navigator.serviceWorker.ready always resolve
-        navigator.serviceWorker.ready.then(function (registration) {
-            console.log('Service worker successfully registered on scope', registration.scope);
-        });
-    });
+  navigator.serviceWorker.register('/veenaw.github.io/sw.js', { scope: '/veenaw.github.io/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
 }
 
 var maximages = 5;
