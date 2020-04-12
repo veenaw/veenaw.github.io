07d3c8f163492329d450eaf3a2badf0c7be5bf32
veenaw <vwagle2@gmail.com>
2020-04-12 12:13:04 +0530

    adding missing functions

--- app.js
+++ app.js
@@ -88,6 +88,22 @@ function fetchImage(pathToResource) {
   .catch(logError);
 }
 
+function logResult(result) {
+  console.log(result);
+}
+
+function logError(error) {
+  console.log('Looks like there was a problem: \n', error);
+}
+
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+  
+
 
 /*
 
